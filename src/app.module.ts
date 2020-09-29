1210c42d9b7242b415ebd207ce9cb899e9cba7ba
Nguyen Dai <trong-dai.nguyen@ant-tech.eu>
2020-09-29 15:35:27 +0700

    (feat): add config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,29 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'nest-db',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'nest-instagram',
-      synchronize: true,
-      entities: [__dirname + '/**/*.entity{.ts,.js}']
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('MYSQL_HOST'),
+        port: configService.get<number>('PORT_DB'),
+        username: configService.get<string>('MYSQL_USER'),
+        password: configService.get<string>('MYSQL_PASSWORD'),
+        database: configService.get<string>('MYSQL_DATABASE'),
+        synchronize: true,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
+      imports: [ConfigModule],
+      inject: [ConfigService],
     }),
     UserModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import chalk from 'chalk';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8088);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT_SERVER');
+  console.log('PORT :>> ', PORT);
+  await app.listen(PORT);
+  // Logger.log(
+  //   `🚀  Server is listening on port ${chalk.hex(`#87e8de`).bold(`${port}`)}`,
+  //   'Bootstrap',
+  // );
 }
 bootstrap();
