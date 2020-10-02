398212f2df5d8c9ee014f4aa5aca6138b6228a63
Dai Nguyen <ngtrdai290197@gmail.com>
2020-10-02 18:20:45 +0700

    chore(): small fixed

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,27 +7,27 @@ import { User } from './user.entity';
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(User) private userRepository: Repository<User>,
+    @InjectRepository(User) private userRepo: Repository<User>,
   ) { }
 
   create(newUser: CreateUserDto): Promise<User> {
-    return this.userRepository.save(newUser)
+    return this.userRepo.save(newUser)
   }
 
   async update(update: EditUserDto): Promise<User> {
-    await this.userRepository.update(update.id, update)
-    return this.userRepository.findOne(update.id)
+    await this.userRepo.update(update.id, update)
+    return this.userRepo.findOne(update.id)
   }
 
   findAll(): Promise<User[]> {
-    return this.userRepository.find();
+    return this.userRepo.find();
   }
 
   findOne(id: string): Promise<User> {
-    return this.userRepository.findOne(id);
+    return this.userRepo.findOne(id);
   }
 
   async remove(id: string): Promise<void> {
-    await this.userRepository.delete(id);
+    await this.userRepo.delete(id);
   }
 }
